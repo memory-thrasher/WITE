78429592c37c429fc91f16fdbda1c0e860e04166
Sidney Kelley <memory.thrasher@gmail.com>
2023-09-27 22:08:54 -0700

    Progress

---
+++ WITE/ExportOnion.hpp
@@ -0,0 +1,15 @@
+namespace WITE::Export {
+
+  template<LiteralList<resource> R, LiteralList<shader> S>
+  struct ExportOnion {
+
+    template<outputDescription O> auto createTarget() {
+      //TODO;
+    };
+
+    template<size_t idx> auto createSource() {
+    };
+
+  };
+
+}

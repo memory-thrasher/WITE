6fe95c11e54af5275a8838938ad822da1d6af779
Sidney Kelley <memory.thrasher@gmail.com>
2024-01-15 08:46:25 -0700

    Cleanup

--- TestCube/compileShaders.bat
+++
@@ -1,3 +0,0 @@
-powershell -Command "& { Get-ChildItem shaders | Where-Object { $_ -notmatch """.*\.spv""" -and ( !(Test-Path -path shaders\$_.spv) -or ((Get-ChildItem -Path shaders\$_).LastWriteTime -gt (Get-ChildItem -Path shaders\$_.spv).LastWriteTime) ) } } | Foreach-Object { glslangValidator -V shaders\$_ -o shaders\$_.spv }" >compileShaders.log
-rem    -or (Get-ChildItem -Path shaders\$_ -LastWriteTime) -gt (Get-ChildItem -Path shaders\$_.spv -LastWriteTime)
-rem  | Foreach-Object { glslangValidator -V shaders\$_ -o shaders\$_.spv }
--- TestCube/makeApiDump.bat
+++
@@ -1,8 +0,0 @@
-set VK_LAYER_PATH=%VULKAN_SDK%\Source\lib
-set PATH=%PATH%;%VULKAN_SDK%\Third-Party\Bin
-set VK_INSTANCE_LAYERS=VK_LAYER_LUNARG_api_dump
-set VK_APIDUMP_NO_ADDR=true
-set VK_APIDUMP_LOG_FILENAME=C:\Wafflecat\spatial_progression\SpatialProgression\TestCube\apidump_wite.txt
-..\x64\Debug\TestCube.exe
-set VK_APIDUMP_LOG_FILENAME=C:\Wafflecat\spatial_progression\SpatialProgression\TestCube\apidump_baseline.txt
-"C:\Users\sid\Documents\Visual Studio 2017\Projects\VS_Cube\x64\Debug\VS_Cube"
